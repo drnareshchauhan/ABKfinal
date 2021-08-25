91171a8ec37b8cba24089935296196dff263cb9e
drnareshchauhan <drnareshchauhan@gmail.com>
2021-08-25 16:29:03 +0530

    abhay final

---
+++ dist/precache-manifest.279c3b1d78f17c74498d51c487ea0f46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c21901b199a1efd5cca068397e240d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2df2c0abd9d94f86e3f7",
+    "url": "/static/css/main.a0895693.chunk.css"
+  },
+  {
+    "revision": "5a43be1666cc939ded3e",
+    "url": "/static/js/2.c2b343d5.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.c2b343d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df2c0abd9d94f86e3f7",
+    "url": "/static/js/main.5c5698d0.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "35afe9e2d1710dd5b39f6de6125eff54",
+    "url": "/static/media/book.35afe9e2.png"
+  },
+  {
+    "revision": "a0a02c9ba3b9d2ca04eff3f395e50fd2",
+    "url": "/static/media/notebook.a0a02c9b.png"
+  }
+]);
